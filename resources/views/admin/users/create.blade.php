69e44a603ed4706d2bc01941f00c3e54d962fff9
Daniel Poliak <daniel.poliak94@gmail.com>
2018-03-14 17:12:40 +0100

    added select options for role and status

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -5,4 +5,32 @@
 
 <h1>Create Users</h1>
 
+
+{!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+
+  <div class="form-group">
+    {!! Form::label('name', 'Name:') !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+  </div>
+
+  <div class="form-group">
+    {!! Form::label('email', 'Email:') !!}
+    {!! Form::email('email', null, ['class' => 'form-control']) !!}
+  </div>
+
+  <div class="form-group">
+    {!! Form::label('role_id', 'Role:') !!}
+    {!! Form::select('role_id', [''=>'Choose Option'] + $roles, null, ['class' => 'form-control']) !!}
+  </div>
+
+  <div class="form-group">
+    {!! Form::label('status', 'Status:') !!}
+    {!! Form::select('status', array(1 => 'Active', 0 => 'Not Active') , 0, ['class' => 'form-control']) !!}
+  </div>
+
+  <div class="form-group">
+    {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+  </div>
+
+  {!! Form::close() !!}
 @stop
